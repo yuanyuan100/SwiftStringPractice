aa1f079db083fe059dca89201eca45f00dcce0e2
aiWen_Company <yuanyuan_pyy@163.com>
2017-04-26 19:44:59 +0800

    构造函数
    Signed-off-by: aiWen_Company <yuanyuan_pyy@163.com>

---
+++ SwiftStringPractice/PRAInit.swift
@@ -0,0 +1,9 @@
+//
+//  PRAInit.swift
+//  SwiftStringPractice
+//
+//  Created by Snake on 2017/4/26.
+//  Copyright © 2017年 IAsk. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftStringPractice/PRAString.swift
@@ -0,0 +1,9 @@
+//
+//  PRAString.swift
+//  SwiftStringPractice
+//
+//  Created by Snake on 2017/4/26.
+//  Copyright © 2017年 IAsk. All rights reserved.
+//
+
+import Foundation
